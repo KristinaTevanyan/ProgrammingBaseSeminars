fd5aba139849e672f8d58bcd609ef8613108bf75
Christina <kristina.tevanyan@mail.ru>
2022-11-16 23:10:33 +0700

    first commit

---
+++ Seminar_1/ConsoleApp_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("введите первое число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+  if(num1>num2)
+  {
+   Console.WriteLine("максимальным является число: " +num1);
+   Console.WriteLine("максимальным является число: " +num2);
+  }
+  else
+  {
+   Console.WriteLine("максимальным является число: " +num1);
+   Console.WriteLine("максимальным является число: " +num2);
+
+  }
