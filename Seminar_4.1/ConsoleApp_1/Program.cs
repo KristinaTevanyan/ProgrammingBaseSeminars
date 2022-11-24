49c9f159ebd0cbaf9893c9e9dc67e1d50793426e
Christina <kristina.tevanyan@mail.ru>
2022-11-24 22:24:45 +0700

    Add .ConsoleApp_3

--- Seminar_4.1/ConsoleApp_1/Program.cs
+++ Seminar_4.1/ConsoleApp_1/Program.cs
@@ -1,13 +1,13 @@
-﻿// 
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.WriteLine("введите число");
-int i = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
 
-while (i > 0)
+for (int i = 1; i < b; i++)
 {
-int num = i % 10;
-i = i / 10;
-sum = sum + num;
+step = step * a;
 }
-Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+Console.WriteLine("A в степени B равно: " + step);
